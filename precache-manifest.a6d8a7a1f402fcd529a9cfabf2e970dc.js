b43c8a7aa93ba3a9d20a03da1baf224bbaba93c5
MY2333 <66840042+MY2333@users.noreply.github.com>
2020-07-31 19:54:34 -0400

    1

---
+++ precache-manifest.a6d8a7a1f402fcd529a9cfabf2e970dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4334b095516228da2f67d21f773a9e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b1c5e2694e8e54dc5b5",
+    "url": "/static/css/main.1558cc10.chunk.css"
+  },
+  {
+    "revision": "87c76d5a9bc29014741a",
+    "url": "/static/js/2.3a0011e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a0011e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1c5e2694e8e54dc5b5",
+    "url": "/static/js/main.79f08537.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
